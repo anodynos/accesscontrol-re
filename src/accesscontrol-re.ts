5151cb45b2ba279d094c721643788f8d976a893f
Angelos Pikoulas <agelos.pikoulas@gmail.com>
2019-09-19 16:42:25 +0100

    feat(): BREAKING CHANGE build() doesnt give return you the `AccessControl` instance, it's instead kept private (and actually returns you the AccessControlRe instance). The you use `AccessControlRe::permission` instead, which delegates to `AccessControl::permission`

--- src/accesscontrol-re.ts
+++ src/accesscontrol-re.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 import * as _f from 'lodash/fp';
-import { AccessControl, IAccessInfo } from 'accesscontrol';
+import { AccessControl, IAccessInfo, IQueryInfo, Permission } from 'accesscontrol';
 
 /*
 Issue: Dynamic Actions and Possession Groups https://github.com/onury/accesscontrol/issues/46
@@ -21,27 +21,40 @@ Note: accesscontrol/lib/enums = {
 import { Action, actions, Possession, possessions } from 'accesscontrol/lib/enums';
 const crudActions = [...actions];
 
+// note: patched actions of AC remain, irrespective of added AC-re instances
+const addCustomActions = (customActions: string[]) => {
+  _.each(_.uniq(customActions), customAction => {
+    customAction = customAction.toLowerCase();
+    Action[customAction.toUpperCase()] = customAction;
+
+    if (!actions.includes(customAction)) actions.push(customAction);
+  });
+};
+
 export class AccessControlRe {
   private accessInfos: IAccessInfo[] = [];
+  private _accessControl: AccessControl = new AccessControl();
 
-  public addAccessInfo = (accessInfo: IAccessInfo | IAccessInfo[]): void => {
+  public addAccessInfo = (accessInfo: IAccessInfo | IAccessInfo[]): AccessControlRe => {
     if (!accessInfo || !_.isObject(accessInfo))
       throw new Error(`Invalid accessInfo:\n${JSON.stringify(accessInfo)}`);
+
     _.isArray(accessInfo)
       ? this.accessInfos.push(...accessInfo)
       : this.accessInfos.push(accessInfo);
+
+    return this;
   };
 
   public build = _.once(
-    (): AccessControl => {
+    (): AccessControlRe => {
       const actions = this.getActions();
       const resources = this.getResources();
       const roles = this.getRoles();
 
       // allow custom actions (i.e patch AccessControl) - see note in method
-      this.addCustomActions(actions);
+      addCustomActions(actions);
 
-      const ac: AccessControl = new AccessControl();
       for (const ai of this.accessInfos) {
         const [action, actionPossession] = ai.action.split(':');
         // arrayize ai.role
@@ -62,24 +75,25 @@ export class AccessControlRe {
               role: rolesToGrant,
             };
 
-            accessInfo.denied ? ac.deny(accessInfo) : ac.grant(accessInfo);
+            accessInfo.denied
+              ? this._accessControl.deny(accessInfo)
+              : this._accessControl.grant(accessInfo);
           }
         }
       }
 
-      return ac.lock();
+      this._accessControl.lock();
+
+      return this;
     }
   );
 
-  // note: patched actions of AC remain, irrespective of added AC-re instances
-  addCustomActions = (customActions: string[]) => {
-    _.each(_.uniq(customActions), customAction => {
-      customAction = customAction.toLowerCase();
-      Action[customAction.toUpperCase()] = customAction;
-
-      if (!actions.includes(customAction)) actions.push(customAction);
-    });
-  };
+  // Instead of calling `accessControl.permission()` we force use to delegate it to us.
+  // This allows us to do things like solving the empty roles throwing of AccessControl
+  // @see node_modules/accesscontrol/lib/AccessControl.d.ts
+  public permission(queryInfo: IQueryInfo): Permission {
+    return this._accessControl.permission(queryInfo);
+  }
 
   // Some helpers
 
